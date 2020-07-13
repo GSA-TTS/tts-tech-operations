1e689e4eddad46da71ce71a621ad424322d5ba4c
Aidan Feldman <aidan.feldman@gsa.gov>
2020-07-13 18:24:04 -0400

    specify where the issue template comes from

--- terraform/repo/main.tf
+++ terraform/repo/main.tf
@@ -1,7 +1,12 @@
 resource "github_repository_file" "issue_templates" {
   for_each = toset(var.issue_templates)
 
-  repository = var.repo
-  file       = ".github/ISSUE_TEMPLATE/${each.key}"
-  content    = file("${path.module}/../../.github/ISSUE_TEMPLATE/${each.key}")
+  repository     = var.repo
+  file           = ".github/ISSUE_TEMPLATE/${each.key}"
+  content        = file("${path.module}/../../.github/ISSUE_TEMPLATE/${each.key}")
+  commit_message = "updated from canonical source\n\nhttps://github.com/18F/tts-tech-portfolio/blob/master/.github/ISSUE_TEMPLATE/${each.key}"
+
+  lifecycle {
+    ignore_changes = [commit_message]
+  }
 }
