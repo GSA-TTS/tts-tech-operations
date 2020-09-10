5a4e5eddd44e4fe83244040a821b67175ba556d1
Aidan Feldman <aidan.feldman@gsa.gov>
2020-09-10 01:02:25 -0400

    print partner channels

--- slack-inspector/channels.js
+++ slack-inspector/channels.js
@@ -4,7 +4,10 @@ const { WebClient } = require("@slack/web-api");
 
 const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 
-async function fetchConversations() {
+const isPartnerChannel = (channel) =>
+  /-(partners|prtnrs)$/i.test(channel.name_normalized) || channel.is_ext_shared;
+
+const fetchConversations = async () => {
   const channels = [];
 
   // https://slack.dev/node-slack-sdk/web-api#pagination
@@ -13,7 +16,11 @@ async function fetchConversations() {
     Array.prototype.push.apply(channels, page.channels);
   }
 
-  console.log(channels);
-}
+  channels.forEach((channel) => {
+    if (isPartnerChannel(channel)) {
+      console.log(channel.name_normalized);
+    }
+  });
+};
 
 fetchConversations();
