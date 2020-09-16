e32891ce9e1f9d09368c975a6f01b2681a16bd99
Aidan Feldman <aidan.feldman@gsa.gov>
2020-09-16 04:22:24 -0400

    find channels for repositories with vulnerabilities

--- slack-inspector/pr-nudges.js
+++ slack-inspector/pr-nudges.js
@@ -1,18 +1,65 @@
 require("dotenv").config();
 
 const countBy = require("lodash.countby");
+const { graphql } = require("@octokit/graphql");
 const { WebClient } = require("@slack/web-api");
 
 const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 
-const run = async () => {
-  const result = await web.search.messages({
-    query: "https://github.com/18F/ReVAL",
-  });
-  console.log(result.messages.matches);
+const fetchRepos = (org) =>
+  graphql(
+    `
+      query lastIssues($org: String!) {
+        organization(login: $org) {
+          repositories(first: 10) {
+            edges {
+              node {
+                name
+                isArchived
+                vulnerabilityAlerts {
+                  totalCount
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      org,
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    }
+  );
 
-  const counts = countBy(result.messages.matches, "channel.name");
-  console.log(counts);
+const fetchVulnerableRepos = async (org) => {
+  const results = await fetchRepos(org);
+  const repos = results.organization.repositories.edges.map(
+    (edge) => edge.node
+  );
+
+  const vulnerableRepos = repos.filter(
+    (repo) => !repo.isArchived && repo.vulnerabilityAlerts.totalCount > 0
+  );
+
+  return vulnerableRepos.map((repo) => repo.name);
+};
+
+const run = async () => {
+  const org = "18F";
+  const repos = await fetchVulnerableRepos(org);
+  console.log(repos);
+
+  repos.forEach(async (repo) => {
+    const result = await web.search.messages({
+      query: `https://github.com/${org}/${repo}`,
+    });
+    console.log(result.messages.matches);
+
+    const counts = countBy(result.messages.matches, "channel.name");
+    console.log(counts);
+  });
 };
 
 run();
