3b4a502ac204f17620ce5787606b6acc84eac479
Aidan Feldman <aidan.feldman@gsa.gov>
2020-09-28 21:50:01 -0400

    write shared pagination code

--- inspector/guests.js
+++ inspector/guests.js
@@ -1,30 +1,38 @@
-const { botClient } = require("./slack");
+const sortBy = require("lodash.sortby");
+const { paginate } = require("./slack");
 
 const getGuestIDs = async () => {
   const guestIDs = new Set();
-  for await (const usersPage of botClient.paginate("users.list")) {
-    for (const user of usersPage.members) {
-      // a.k.a. single-channel guest
-      if (user.is_ultra_restricted) {
-        guestIDs.add(user.id);
-      }
+  for await (const user of paginate("users.list", "members")) {
+    // a.k.a. single-channel guest
+    if (user.is_ultra_restricted) {
+      guestIDs.add(user.id);
     }
   }
   return guestIDs;
 };
 
+async function* getPublicChannels() {
+  const channels = paginate("conversations.list", "channels", {
+    exclude_archived: true,
+    types: "public_channel",
+  });
+
+  for await (const channel of channels) {
+    yield channel;
+  }
+}
+
 const getNumGuests = async (channelID, allGuestIDs) => {
   let numGuests = 0;
 
-  const memberPages = botClient.paginate("conversations.members", {
+  const memberIDs = paginate("conversations.members", "members", {
     channel: channelID,
   });
 
-  for await (const memberPage of memberPages) {
-    for (const memberID of memberPage.members) {
-      if (allGuestIDs.has(memberID)) {
-        numGuests += 1;
-      }
+  for await (const memberID of memberIDs) {
+    if (allGuestIDs.has(memberID)) {
+      numGuests += 1;
     }
   }
 
@@ -35,37 +43,40 @@ const getChannelInfo = async () => {
   const guestIDs = await getGuestIDs();
   const channels = [];
 
-  const conversationPages = botClient.paginate("conversations.list", {
-    exclude_archived: true,
-    types: "public_channel",
-  });
+  for await (const channel of getPublicChannels()) {
+    let numGuests;
 
-  for await (const conversationPage of conversationPages) {
-    for (const channel of conversationPage.channels) {
-      let numGuests;
+    try {
+      numGuests = await getNumGuests(channel.id, guestIDs);
+    } catch (err) {
+      console.error(`Failed to fetch members for ${channel.name}`);
+      // console.error(err);
+    }
 
-      try {
-        numGuests = await getNumGuests(channel.id, guestIDs);
-      } catch (err) {
-        console.error(`Failed to fetch members for ${channel.name}`);
-        // console.error(err);
-      }
-
-      if (numGuests > 0) {
-        channels.push({
-          name: channel.name,
-          num_guests: numGuests,
-        });
-      }
+    if (numGuests > 0) {
+      channels.push({
+        name: channel.name,
+        num_guests: numGuests,
+      });
     }
   }
 
   return channels;
 };
 
-(async () => {
+const run = async () => {
   const channels = await getChannelInfo();
   sortBy(channels, ["num_guests"]).forEach((channel) =>
     console.log(channel.name, channel.num_guests)
   );
-})();
+};
+
+// https://nodejs.org/docs/latest/api/modules.html#modules_accessing_the_main_module
+if (require.main === module) {
+  run();
+} else {
+  module.exports = {
+    getGuestIDs,
+    getPublicChannels,
+  };
+}
--- inspector/slack.js
+++ inspector/slack.js
@@ -3,7 +3,22 @@ require("dotenv").config();
 const { WebClient } = require("@slack/web-api");
 
 // both are needed, since search is only possible with a user token (https://api.slack.com/methods/search.messages#facts) but we want messages to come from the bot
+const userClient = new WebClient(process.env.SLACK_USER_TOKEN);
+const botClient = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+const paginate = async function* paginate(method, key, options) {
+  options = options || {};
+  const pages = botClient.paginate(method, options);
+
+  for await (const page of pages) {
+    for (const item of page[key]) {
+      yield item;
+    }
+  }
+};
+
 module.exports = {
-  userClient: new WebClient(process.env.SLACK_USER_TOKEN),
-  botClient: new WebClient(process.env.SLACK_BOT_TOKEN),
+  userClient,
+  botClient,
+  paginate,
 };
