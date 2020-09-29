c8f2826244ab3b89cf85a27083ec324ebc1424eb
Aidan Feldman <aidan.feldman@gsa.gov>
2020-09-29 00:52:04 -0400

    add greater rate limit for SCIM

--- inspector/deactivate.js
+++ inspector/deactivate.js
@@ -6,16 +6,19 @@ const guests = require("./guests");
 const THIRTY_DAYS_AGO = Date.now() / 1000 - 60 * 60 * 24 * 30;
 
 const shouldDeactivate = async (user) => {
-  // there isn't a good way to see if they were recently invited outside of Enterprise Grid, so just go based on a not-recently updated time
-  if (user.updated < THIRTY_DAYS_AGO) {
-    const result = await botClient.users.conversations({
-      user: user.id,
-      limit: 1,
-    });
+  // there isn't a good way to see if they were recently invited (outside of Enterprise Grid), so just go based on their update time
+  if (user.updated > THIRTY_DAYS_AGO) {
+    return false;
+  }
 
-    if (result.channels.length === 0) {
-      return true;
-    }
+  const result = await botClient.users.conversations({
+    user: user.id,
+    limit: 1,
+  });
+
+  if (result.channels.length === 0) {
+    // single-channel guest that was presumably in a channel that has been archived
+    return true;
   }
 
   return false;
@@ -29,16 +32,17 @@ const deactivate = (userID) =>
 const run = async () => {
   const guestsByID = await guests.getGuestsByID();
 
-  // do in serial to avoid rate limits
+  // do in serial with delays to avoid rate limits
   for (const id in guestsByID) {
     const user = guestsByID[id];
     const deactivateUser = await shouldDeactivate(user);
     if (deactivateUser) {
       console.log(id, user.profile.real_name);
       await deactivate(id);
+      await delay(1000);
     }
 
-    await delay(300);
+    await delay(500);
   }
 };
 
