9180d0263b7aeca3f9399a349ed946ba95b00b5b
Aidan Feldman <aidan.feldman@gsa.gov>
2021-04-13 17:20:28 -0400

    put labels in logical order

--- terraform/repo/vars.tf
+++ terraform/repo/vars.tf
@@ -24,23 +24,27 @@ locals {
     }
 
     # grooming states
-    "g: accepted" = {
+    "g: initial" = {
       color       = local.label_colors.grooming
-      description = "Issue has been fully groomed."
+      description = "Issue template needs to be filled out, and/or initiative/timing labels need to be added."
     }
     "g: final" = {
       color       = local.label_colors.grooming
       description = "Issue ready for review."
     }
-    "g: initial" = {
+    "g: accepted" = {
       color       = local.label_colors.grooming
-      description = "Issue template needs to be filled out, and/or initiative/timing labels need to be added."
+      description = "Issue has been fully groomed."
     }
 
     # initiatives
     "i: custom software" = {
       color       = local.label_colors.initiatives
       description = "Relating to systems we've built in TTS"
+    }
+    "i: customer support" = {
+      color       = local.label_colors.initiatives
+      description = "Relating to supporting TTS staff and partners: troubleshooting, documentation, etc."
     }
     "i: infrastructure" = {
       color       = local.label_colors.initiatives
@@ -57,10 +61,6 @@ locals {
     "i: SaaS" = {
       color       = local.label_colors.initiatives
       description = "Relating to third-party software: acquisition, security compliance, etc."
-    }
-    "i: customer support" = {
-      color       = local.label_colors.initiatives
-      description = "Relating to supporting TTS staff and partners: troubleshooting, documentation, etc."
     }
     # deprecated
     "i: DEI" = {
@@ -89,25 +89,26 @@ locals {
       color       = "ea3546"
       description = "Has a hard or soft deadline"
     }
-    "t: days" = {
-      color       = local.label_colors.timing
-      description = "Should be complete-able in a matter of days (wall clock time)"
-    }
     "t: expedite" = {
       color       = local.label_colors.timing
       description = "Needed to be pulled into a sprint after Planning was complete"
     }
+    # estimates
     "t: hours" = {
       color       = local.label_colors.timing
       description = "Should be complete-able in a matter of hours (wall clock time)"
     }
-    "t: months" = {
+    "t: days" = {
       color       = local.label_colors.timing
-      description = "Should be complete-able in a matter of months (wall clock time) — should be split up"
+      description = "Should be complete-able in a matter of days (wall clock time)"
     }
     "t: weeks" = {
       color       = local.label_colors.timing
       description = "Should be complete-able in a matter of weeks (wall clock time) — see what can be split out"
+    }
+    "t: months" = {
+      color       = local.label_colors.timing
+      description = "Should be complete-able in a matter of months (wall clock time) — should be split up"
     }
   }
 }
