c8a035a19ffe462903a41fcc10b12499d61e5564
Aidan Feldman <aidan.feldman@gsa.gov>
2021-04-14 01:22:53 -0400

    refactor label renaming script

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "active_repos" {
+  description = "Used by rename_label.py"
   value = [for repo, config in local.active_repos : repo]
 }
--- terraform/rename_label.py
+++ terraform/rename_label.py
@@ -2,12 +2,19 @@ import json
 import subprocess
 import sys
 
-outputs = json.loads(subprocess.check_output(["terraform", "output", "-json"]))
-repos = outputs["active_repos"]["value"]
+
+def label_resource(repo, label):
+    return f'module.repo["{repo}"].github_issue_label.labels["{label}"]'
+
+
+def get_repos():
+    outputs = json.loads(subprocess.check_output(["terraform", "output", "-json"]))
+    return outputs["active_repos"]["value"]
+
 
 before = sys.argv[1]
 after = sys.argv[2]
 
-for repo in repos:
-    cmd = f'terraform state mv \'module.repo["{repo}"].github_issue_label.labels["{before}"]\' \'module.repo["{repo}"].github_issue_label.labels["{after}"]\''
+for repo in get_repos():
+    cmd = f"terraform state mv '{label_resource(repo, before)}' '{label_resource(repo, after)}'"
     print(cmd)
