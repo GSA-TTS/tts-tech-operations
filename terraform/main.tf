ff8df4b63c73447b4771e79b683758934218d19d
John Jediny <JJediny@users.noreply.github.com>
2021-06-25 11:29:56 -0400

    terraform fmt requires short version form with ~>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.0"
   required_providers {
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
       version = "~> 4.0"
     }
   }
