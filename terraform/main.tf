4cf91a8b46f26a685ef5d8342d44977d61378c5f
John Jediny <JJediny@users.noreply.github.com>
2021-06-25 11:06:47 -0400

    Bump terraform and provider versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
 terraform {
-  required_version = "~> 0.14.0"
+  required_version = "~> 1.0.0"
   required_providers {
     github = {
       source = "integrations/github"
+      version = "~> 4.0"
     }
   }
   backend "s3" {
