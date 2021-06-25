9ab136336a3f174a7757c91e2198c4e215f5f038
John Jediny <JJediny@users.noreply.github.com>
2021-06-25 11:43:06 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
       version = "~> 4.0"
     }
   }
